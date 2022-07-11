671ac447357314c1fec686590af99b227bcbd83e
andreyDanilenko <danilenko.a.g@mail.ru>
2022-07-11 10:27:51 +0300

    add index

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'src/components/app/app';
+import App from 'src/components/app';
 
 const Setting = {
   RENTAL_COUNT: 30,
