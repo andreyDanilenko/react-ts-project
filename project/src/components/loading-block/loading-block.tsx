12c24d8747c20a48b07b86508cdc18997119f956
andreyDanilenko <danilenko.a.g@mail.ru>
2022-09-04 11:44:06 +0300

    add loading block

---
+++ project/src/components/loading-block/loading-block.tsx
@@ -0,0 +1,10 @@
+import './loading-block.css';
+
+const LoadingBlock = () => (
+  <div className="loading-block">
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+  </div>
+
+);
+
+export default LoadingBlock;
