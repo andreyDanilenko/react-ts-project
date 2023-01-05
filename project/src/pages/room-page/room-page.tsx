2a46d53032f05e0356c96b8cd72398bfb505e5b0
danilko.a.g@ <danilko.a.g@gmail.com>
2023-01-05 22:58:25 +0300

    add data for room page

--- project/src/pages/room-page/room-page.tsx
+++ project/src/pages/room-page/room-page.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { FormSubmit, ReviewList, Map, PlacesList, LoadingBlock } from 'src/components';
 import { useAppDispatch, useAppSelector } from 'src/hooks';
+import { getUpperCase } from 'src/utils/utils';
 import { reviews } from 'src/mocks/comments';
 import { fetchOffer } from 'src/store/api-action';
 import { Offer } from 'src/types/offers';
@@ -12,11 +13,11 @@ type Props = {
 
 const RoomPage = (props: Props): JSX.Element => {
   const { id } = useParams();
-  const { offer, loadingOfferAction } = useAppSelector((state)=> state);
+  const { offer, loadingOfferAction } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
 
 
-  useEffect(()=> {
+  useEffect(() => {
     if (id) {
       dispatch(fetchOffer(id));
     }
@@ -25,12 +26,26 @@ const RoomPage = (props: Props): JSX.Element => {
   // eslint-disable-next-line no-console
   console.log('component', offer);
 
+  const getImagePreviewItem = (image: string): JSX.Element =>
+    (
+      <div className="property__image-wrapper">
+        <img className="property__image" src={image} alt="Photo_studio" />
+      </div>
+    );
+
+  const getSubjectItem = (subject: string): JSX.Element =>
+    (
+      <li className="property__inside-item">
+        {subject}
+      </li>
+    );
 
   if (loadingOfferAction) {
     return <LoadingBlock />;
   }
   // eslint-disable-next-line no-console
   console.log('offer', offer);
+
   const city = props.offers[0].city;
 
   return (
@@ -38,38 +53,22 @@ const RoomPage = (props: Props): JSX.Element => {
       <section className="property">
         <div className="property__gallery-container container">
           <div className="property__gallery">
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/room.jpg" alt="Photo_studio" />
-            </div>
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo_studio" />
-            </div>
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-02.jpg" alt="Photo_studio" />
-            </div>
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-03.jpg" alt="Photo_studio" />
-            </div>
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/studio-01.jpg" alt="Photo_studio" />
-            </div>
-            <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo_studio" />
-            </div>
+            {
+              offer?.images.slice(0, 6).map((image) => getImagePreviewItem(image))
+            }
           </div>
         </div>
         <div className="property__container container">
           <div className="property__wrapper">
-
-            <div className="property__mark">
-              <span>Premium</span>
-            </div>
-
+            {offer?.isPremium &&
+              <div className="property__mark">
+                <span>Premium</span>
+              </div>}
             <div className="property__name-wrapper">
               <h1 className="property__name">
-              Beautiful &amp; luxurious studio at great location
+                Beautiful &amp; luxurious studio at great location
               </h1>
-              <button className="property__bookmark-button button" type="button">
+              <button className="property__bookmark-button  button" type="button">
                 <svg className="property__bookmark-icon" width="31" height="33">
                   <use xlinkHref="#icon-bookmark"></use>
                 </svg>
@@ -78,59 +77,35 @@ const RoomPage = (props: Props): JSX.Element => {
             </div>
             <div className="property__rating rating">
               <div className="property__stars rating__stars">
-                <span style={{ width: '80%' }}></span>
+                {offer?.rating && <span style={{ width: `${offer?.rating / 5 * 100}%` }}></span>}
                 <span className="visually-hidden">Rating</span>
               </div>
-              <span className="property__rating-value rating__value">4.8</span>
+              {offer?.rating && <span className="property__rating-value rating__value">{offer?.rating}</span>}
             </div>
             <ul className="property__features">
               <li className="property__feature property__feature--entire">
-              Apartment
+                {offer?.type && getUpperCase(offer.type)}
               </li>
               <li className="property__feature property__feature--bedrooms">
-              3 Bedrooms
+
+                {offer?.bedrooms && `${offer.bedrooms} Bedrooms`}
               </li>
-              <li className="property__feature property__feature--adults">
-              Max 4 adults
+              <li className="property__feature property__feature--agdults">
+                {offer?.maxAdults && `Max ${offer.maxAdults} adults`}
               </li>
             </ul>
             <div className="property__price">
-              <b className="property__price-value">&euro;120</b>
+              <b className="property__price-value">&euro;
+                {offer?.price && offer.price}
+              </b>
               <span className="property__price-text">&nbsp;night</span>
             </div>
             <div className="property__inside">
               <h2 className="property__inside-title">What&apos;s inside</h2>
               <ul className="property__inside-list">
-                <li className="property__inside-item">
-                Wi-Fi
-                </li>
-                <li className="property__inside-item">
-                Washing machine
-                </li>
-                <li className="property__inside-item">
-                Towels
-                </li>
-                <li className="property__inside-item">
-                Heating
-                </li>
-                <li className="property__inside-item">
-                Coffee machine
-                </li>
-                <li className="property__inside-item">
-                Baby seat
-                </li>
-                <li className="property__inside-item">
-                Kitchen
-                </li>
-                <li className="property__inside-item">
-                Dishwasher
-                </li>
-                <li className="property__inside-item">
-                Cabel TV
-                </li>
-                <li className="property__inside-item">
-                Fridge
-                </li>
+                {
+                  offer?.goods.slice(0, 6).map((subject) => getSubjectItem(subject))
+                }
               </ul>
             </div>
             <div className="property__host">
@@ -140,37 +115,38 @@ const RoomPage = (props: Props): JSX.Element => {
                   <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
                 </div>
                 <span className="property__user-name">
-                Angelina
+                  Angelina
                 </span>
                 <span className="property__user-status">
-                Pro
+                  Pro
                 </span>
               </div>
               <div className="property__description">
                 <p className="property__text">
-                A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+                  A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
                 </p>
                 <p className="property__text">
-                An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
+                  An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
                 </p>
               </div>
             </div>
             <section className="property__reviews reviews">
-              <ReviewList reviews={reviews}/>
+              <ReviewList reviews={reviews} />
               <FormSubmit />
             </section>
           </div>
         </div>
         <section className="property__map map">
-          <Map city={city} offers={props.offers} selectedPoint={undefined}/>
+          <Map city={city} offers={props.offers} selectedPoint={undefined} />
         </section>
       </section>
       <div className="container">
         <section className="near-places places">
-          <PlacesList offers={props.offers.slice(0, 3)} title="Other places in the neighbourhood"/>
+          <PlacesList offers={props.offers.slice(0, 3)} title="Other places in the neighbourhood" />
         </section>
       </div>
     </main>
-  );};
+  );
+};
 
 export default RoomPage;
