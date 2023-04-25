fa1ea4775da84c6ff7a8899d2d48087114726ecd
adanilenko <a.danilenko@platim.ru>
2023-04-25 13:42:40 +0300

    fix review item

--- project/src/components/review-item/review-item.tsx
+++ project/src/components/review-item/review-item.tsx
@@ -1,36 +1,29 @@
 import React from 'react';
 import { Review } from 'src/types/offers';
 
-type Props = {
-   review: Review
-}
-
-const ReviewItem = (props: Props): JSX.Element => {
-  const {comment, date, rating, user} = props.review;
-
-  return (
-    <li className="reviews__item">
-      <div className="reviews__user user">
-        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
-        </div>
-        <span className="reviews__user-name">
-          { user.name }
-        </span>
+const ReviewItem = ({comment, date, rating, user}: Review): JSX.Element => (
+  <li className="reviews__item">
+    <div className="reviews__user user">
+      <div className="reviews__avatar-wrapper user__avatar-wrapper">
+        <img className="reviews__avatar user__avatar" src={user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
       </div>
-      <div className="reviews__info">
-        <div className="reviews__rating rating">
-          <div className="reviews__stars rating__stars">
-            <span style={{ width: `${rating / 5 * 100}%` }}></span>
-            <span className="visually-hidden">Rating</span>
-          </div>
+      <span className="reviews__user-name">
+        { user.name }
+      </span>
+    </div>
+    <div className="reviews__info">
+      <div className="reviews__rating rating">
+        <div className="reviews__stars rating__stars">
+          <span style={{ width: `${rating / 5 * 100}%` }}></span>
+          <span className="visually-hidden">Rating</span>
         </div>
-        <p className="reviews__text">
-          { comment }
-        </p>
-        <time className="reviews__time" dateTime="2019-04-24">{ date }</time>
       </div>
-    </li>
-  );};
+      <p className="reviews__text">
+        { comment }
+      </p>
+      <time className="reviews__time" dateTime="2019-04-24">{ date }</time>
+    </div>
+  </li>
+);
 
 export default ReviewItem;
